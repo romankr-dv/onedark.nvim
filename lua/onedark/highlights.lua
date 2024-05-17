e037f1627e32c5938edfcc29e31e0723ed82a605
romankr <romankr@default-value.com>
2024-05-17 13:10:13 +0300

    Change quick fix line highlight

--- lua/onedark/highlights.lua
+++ lua/onedark/highlights.lua
@@ -91,7 +91,7 @@ hl.common = {
     WinSeparator = {fg = c.bg3},
     Visual = {bg = c.bg3},
     VisualNOS = {fg = c.none, bg = c.bg2, fmt = "underline"},
-    QuickFixLine = {fg = c.blue, fmt = "underline"},
+    QuickFixLine = {bg = c.bg2},
     Debug = {fg = c.yellow},
     debugPC = {fg = c.bg0, bg = c.green},
     debugBreakpoint = {fg = c.bg0, bg = c.red},
