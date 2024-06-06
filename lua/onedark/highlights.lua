df4e60dfafadda9be223b01c45b847ddad5ef6de
romankr <romankr@default-value.com>
2024-06-06 13:53:20 +0300

    Add telescope multi selection highlight

--- lua/onedark/highlights.lua
+++ lua/onedark/highlights.lua
@@ -570,6 +570,8 @@ hl.plugins.telescope = {
     TelescopePromptPrefix = { fg=c.bg_blue },
     TelescopeSelection = {fg = c.bg_blue, fmt="reverse"},
     TelescopeSelectionCaret = {fg = c.bg_blue, fmt="reverse"},
+    TelescopeMultiSelection = { fg=c.bg_blue, bg=c.bg3 },
+    TelescopeMultiIcon = { fg=c.bg_blue, bg=c.bg3 },
 }
 
 hl.plugins.dashboard = {
