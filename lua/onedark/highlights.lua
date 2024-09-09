59d8f95b935813865fa0b74f20d5def816b24545
romankr <romankr@default-value.com>
2024-09-09 17:20:33 +0300

    Change operator highlight

--- lua/onedark/highlights.lua
+++ lua/onedark/highlights.lua
@@ -190,7 +190,7 @@ if vim.api.nvim_call_function("has", { "nvim-0.8" }) == 1 then
         ["@none"] = colors.Fg,
         ["@number"] = colors.Orange,
         ["@number.float"] = colors.Orange,
-        ["@operator"] = colors.Fg,
+        ["@operator"] = colors.LightGrey,
         ["@parameter.reference"] = colors.Fg,
         ["@property"] = colors.Cyan,
         ["@punctuation.delimiter"] = colors.LightGrey,
