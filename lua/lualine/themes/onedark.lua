f426ad812ccd00544d9d0fab21c2e40f90384972
romankr <romankr@default-value.com>
2024-10-13 13:20:02 +0300

    Revert lualine changes

--- lua/lualine/themes/onedark.lua
+++ lua/lualine/themes/onedark.lua
@@ -1,21 +1,32 @@
 local c = require('onedark.colors')
 local cfg = vim.g.onedark_config
+local colors = {
+    bg = c.bg0,
+    fg = c.fg,
+    red = c.red,
+    green = c.green,
+    yellow = c.yellow,
+    blue = c.blue,
+    purple = c.purple,
+    cyan = c.cyan,
+    gray = c.grey
+}
 
 local one_dark = {
     inactive = {
-        a = {fg = c.grey, bg = c.bg0, gui = 'bold'},
-        b = {fg = c.grey, bg = c.bg0},
-        c = {fg = c.grey, bg = cfg.lualine.transparent and c.none or c.bg0},
+        a = {fg = colors.gray, bg = colors.bg, gui = 'bold'},
+        b = {fg = colors.gray, bg = colors.bg},
+        c = {fg = colors.gray, bg = cfg.lualine.transparent and c.none or colors.bg},
     },
     normal = {
-        a = {fg = c.bg0, bg = c.green, gui = 'bold'},
-        b = {fg = c.fg, bg = c.bg3},
-        c = {fg = c.fg, bg = cfg.lualine.transparent and c.none or c.bg0},
+        a = {fg = colors.bg, bg = colors.green, gui = 'bold'},
+        b = {fg = colors.fg, bg = c.bg3},
+        c = {fg = colors.fg, bg = cfg.lualine.transparent and c.none or colors.bg},
     },
-    visual = {a = {fg = c.bg0, bg = c.purple, gui = 'bold'}},
-    replace = {a = {fg = c.bg0, bg = c.red, gui = 'bold'}},
-    insert = {a = {fg = c.bg0, bg = c.blue, gui = 'bold'}},
-    command = {a = {fg = c.bg0, bg = c.yellow, gui = 'bold'}},
-    terminal = {a = {fg = c.bg0, bg = c.cyan, gui = 'bold'}},
+    visual = {a = {fg = colors.bg, bg = colors.purple, gui = 'bold'}},
+    replace = {a = {fg = colors.bg, bg = colors.red, gui = 'bold'}},
+    insert = {a = {fg = colors.bg, bg = colors.blue, gui = 'bold'}},
+    command = {a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'}},
+    terminal = {a = {fg = colors.bg, bg = colors.cyan, gui = 'bold'}},
 }
 return one_dark;
