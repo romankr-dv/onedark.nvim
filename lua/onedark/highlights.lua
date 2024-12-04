d54dc0f8b4e3bee5e5be0290a9674410e4ad1d91
romankr <romankr@default-value.com>
2024-12-04 15:58:41 +0200

    Revert diffview change

--- lua/onedark/highlights.lua
+++ lua/onedark/highlights.lua
@@ -486,7 +486,7 @@ hl.plugins.diffview = {
     DiffviewFilePanelCounter = {fg = c.purple, fmt = "bold"},
     DiffviewFilePanelFileName = colors.Fg,
     DiffviewNormal = hl.common.Normal,
-    DiffviewNonText = colors.Grey,
+    DiffviewNonText = colors.LightGrey,
     DiffviewCursorLine = hl.common.CursorLine,
     DiffviewVertSplit = hl.common.VertSplit,
     DiffviewSignColumn = hl.common.SignColumn,
