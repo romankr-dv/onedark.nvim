b71353480d9309bd716b371ca7c58274c00d787f
romankr <romankr@default-value.com>
2025-06-30 14:32:06 +0300

    Change winseparator color

--- lua/onedark/highlights.lua
+++ lua/onedark/highlights.lua
@@ -90,7 +90,7 @@ hl.common = {
     TabLine = {fg = c.fg, bg = c.bg1},
     TabLineFill = {fg = c.grey, bg = c.bg1},
     TabLineSel =  {fg = c.fg, bg = c.bg3},
-    WinSeparator = {fg = c.bg3},
+    WinSeparator = {fg = util.blend(c.fg, c.bg1, 0.1)},
     Visual = {bg = c.dark_grey},
     VisualNOS = {fg = c.none, bg = c.bg2, fmt = "underline"},
     QuickFixLine = {fg = c.yellow},
